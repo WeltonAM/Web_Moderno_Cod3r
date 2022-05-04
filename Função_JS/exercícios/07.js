b213edff1f114a4a41007d1b6a71793f78ce755d
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-04 13:54:48 -0300

    bhaskara

---
+++ Função_JS/exercícios/07.js
@@ -0,0 +1,20 @@
+function bhaskara (a, b, c){
+    let resultado = [];
+    let delta = (b ** 2) - (4 * a * c);
+
+    if (delta < 0){
+        return "Delta é negativo"
+    }
+
+    let x1 = (-b + Math.sqrt(delta))/(2 * a);
+    resultado.push(x1);
+    
+    let x2 = (-b - Math.sqrt(delta))/(2 * a);
+    resultado.push(x2);
+
+    return resultado
+
+}
+
+console.log(bhaskara(1, 3, 2));
+console.log(bhaskara(7, 2, 5));
