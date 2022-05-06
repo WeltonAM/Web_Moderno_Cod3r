a79010cc038fc9228eb99e991ab1e6f6dc9caeef
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-05 23:30:48 -0300

    Divisível por 3

---
+++ Função_JS/exercícios/10.js
@@ -0,0 +1,11 @@
+function divisivelPorTres(num){
+    if(num % 3 == 0){
+        console.log(`"${num}" é divisível por 3`)
+    }
+}
+
+divisivelPorTres(723);
+divisivelPorTres(724);
+divisivelPorTres(4);
+divisivelPorTres(3);
+
