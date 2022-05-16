2163acaec943bf06bee792a20fa1b3b0a1940005
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-16 13:58:15 -0300

    Create concat.js

---
+++ Arrays/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Maria Ester', 'Ana Flor'];
+const filhos = ['Bento', 'José Neto'];
+const todos = filhas.concat(filhos, 'Gohan');
+console.log(todos);
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6, 7]]));
