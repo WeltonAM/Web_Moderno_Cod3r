cc5f7f19c62047fa5f3a375aa937f2b76c4a45bf
Welton Almeida de Matos <74322996+WeltonAM@users.noreply.github.com>
2022-05-26 12:50:09 -0300

    Update map1.js

--- 04_Arrays/map1.js
+++ 04_Arrays/map1.js
@@ -8,7 +8,7 @@ console.log(resultado);
 
 const soma10 = e => e + 10;
 const triplo = e => e * 3;
-const paraDinheiro = e => `$R ${parseFloat(e).toFixed(2).replace('.', ',')}`;
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`;
 
 resultado2 = nums.map(soma10).map(triplo).map(paraDinheiro);
 console.log(resultado2)
